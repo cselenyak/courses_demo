d4e2970e799e4bfebfb1299e88f1b0ab4a383850
cselenyaka <cselenyaka@richter.hu>
2021-12-08 21:04:43 +0100

    First commit to git

---
+++ src/main/java/com/cselenyak/CoursesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cselenyak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cselenyak/CoursesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cselenyak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
